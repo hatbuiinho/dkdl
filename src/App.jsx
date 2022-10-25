a66fcee7966c52fb7a709a523ef2154c8815dded
Huy Tuyen <fsoft.datnn@gmail.com>
2022-10-25 23:40:16 +0700

    change router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import './assets/scss/bootstrap.scss';
-import { Festival } from './page/Festival';
-import { AppRoute } from './container/AppRoute';
 import { Switch } from 'react-router-dom';
-import { HeaderLayout } from './container/Layouts/HeaderLayout';
-import CeremonyServingRegister from './component/CeremonyServingRegister';
-import Home from './page/Home';
-import { HomeLayout } from './container/Layouts/HomeLayout';
-import FestivalLayout from './container/Layouts/FestivalLayout';
+import { ROUTES } from './routes';
+import { Route } from 'react-router-dom';
+import { AppRoute } from './routes/AppRoute';
+
 function App() {
   return (
     <Switch>
-      <AppRoute path="/" needAuth={false} component={Home} layout={HomeLayout} exact />
-      <AppRoute
-        path="/festival"
-        needAuth={false}
-        component={Festival}
-        layout={FestivalLayout}
-      />
-      <AppRoute
-        path="/register-result"
-        needAuth={false}
-        component={CeremonyServingRegister}
-        layout={HeaderLayout}
-      />
+      {ROUTES.map((route) => (
+        <AppRoute key={route.key} {...route} />
+      ))}
+      <Route>Page Not Found</Route>
     </Switch>
   );
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import FestivalLayout from '../container/Layouts/FestivalLayout';
+import { HeaderLayout } from '../container/Layouts/HeaderLayout';
+import { HomeLayout } from '../container/Layouts/HomeLayout';
+import { Festival } from '../page/Festival';
+import Home from '../page/Home';
+import RegisterResult from '../page/RegisterResult';
+
+const ROUTES = [
+  {
+    key: 'HOME',
+    path: '/',
+    needAuth: false,
+    exact: true,
+    component: Home,
+    layout: HomeLayout,
+  },
+  {
+    key: 'FESTIVAL',
+    path: '/festival',
+    needAuth: false,
+    component: Festival,
+    layout: FestivalLayout,
+  },
+  {
+    key: 'REGISTER_INFO',
+    path: '/register-info',
+    // needAuth: true,
+    component: RegisterResult,
+    layout: HeaderLayout,
+  },
+];
+
+const Routes = () => {
+  return <div>Routes</div>;
+};
+
+export { ROUTES };
+export default Routes;
