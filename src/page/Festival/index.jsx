5cd5cbadce24fadd48709798478c6dae5c25d2a3
oanhdyno <oanhtt@dyno.vn>
2022-10-27 10:55:49 +0700

    fix style festival

--- src/page/Festival/index.jsx
+++ src/page/Festival/index.jsx
@@ -1,4 +1,4 @@
-import { Tabs } from 'antd';
+import { Col, Row, Tabs } from 'antd';
 // import React from 'react';
 import './style.scss';
 import { Typography, Button } from 'antd';
@@ -7,28 +7,37 @@ import fest_infos from '../../configs/festival';
 // import '../../../assets/images/layouts/festival/BAN_MT.jpg'
 export const Festival = () => {
   return (
-    <div style={{}}>
+    <div
+      style={{
+        padding: '0 50px',
+        justifyContent: 'center',
+      }}
+    >
       {/* <Title style={{ padding: '10px 40px' }}>
         Giới thiệu các ban tham gia Công quả Đại Lễ!
       </Title> */}
       <Tabs defaultActiveKey="0">
         {fest_infos.map((data, i) => (
           <Tabs.TabPane tab={data.tab} key={i}>
+            <Row>
+              <Col span={24}></Col>
+            </Row>
             <div className="festival_item">
-              <Title
+              {/* <Title
                 style={{
                   textAlign: 'center',
                 }}
-              >{`BAN ${data.tab}`}</Title>
+              >{`BAN ${data.tab}`}</Title> */}
 
               <div className="festival_item_body">
                 <div className="container_img">
-                  <img style={{ width: '40%' }} src="/images/festival/BAN_MT.jpg"></img>
+                  <img style={{ width: '63%' }} src="/images/festival/BAN_MT.jpg"></img>
                 </div>
                 <div
                   className="container_content"
                   style={{
                     textAlign: 'justify',
+                    fontSize: '16px',
                   }}
                 >
                   {data.content}
@@ -38,8 +47,28 @@ export const Festival = () => {
                     textAlign: 'center',
                   }}
                 >
-                  <Button shape="round" style={{ background: 'green', height: '60px' }}>
-                    <Title style={{ color: 'white' }}>Đăng ký ngay!</Title>
+                  <Button
+                    style={{
+                      background: 'green',
+                      // background: green,
+                      /* padding: 13px, */
+                      /* display: flex, */
+                      height: 'auto',
+                      padding: '20px 30px',
+                      borderRadius: '50px',
+                    }}
+                  >
+                    <span
+                      style={{
+                        color: 'white',
+                        fontWeight: 600,
+                        fontSize: '16px',
+                        borderRadius: '50px',
+                      }}
+                    >
+                      Đăng ký ngay!
+                    </span>
+                    {/* <Title style={{ color: 'white' }}>Đăng ký ngay!</Title> */}
                   </Button>
                 </div>
               </div>
